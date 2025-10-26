d9e0414acd0e8d012aeeb92e40dae4edc9ecfc1b
Satish Pai <hisatishpai@gmail.com>
2025-10-26 22:31:46 +0000

    SP-001:Making the test work

--- tests/niche.spec.js
+++ tests/niche.spec.js
@@ -1,10 +1,16 @@
 const {test, expect} = require('@playwright/test');
 const LoginPage = require('../pages/loginpage');
+const { log } = require('console');
 
 test('orangehrm login test', async ({page})=>{
     await page.goto('https://nichethyself.com/tourism/home.html');
     await page.viewportSize({width:1920, height:1080});
+    // await page.pause();
+    await page.evaluate(() => {
+         window.scrollBy(0, 1200);
+    });
+    // await page.pause();
    const loginPage = new LoginPage(page);
    await loginPage.loginToPortal();
-   await expect(loginPage).toHaveURL('https://nichethyself.com/tourism/admin/dashboard.php');
+   await expect(page).toHaveURL('https://nichethyself.com/tourism/home.html');
 });
