c4edb2a273d4e19634b0644dab477592ab848bba
Satish Pai <hisatishpai@gmail.com>
2025-10-26 21:03:55 +0000

    first commit

---
+++ tests/orangehrm.spec.js
@@ -0,0 +1,9 @@
+const {expect, test} = require('@playwright/test');
+
+test('orangehrm login test', async ({page})=>{
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    await page.getByPlaceholder('Username').fill('Admin');
+    await page.getByPlaceholder('Password').fill('admin123');
+    await page.getByRole('button', {name: 'Login'}).click();
+    await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index');
+});
